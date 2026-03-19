66c06c46e0064b6dc15531f5899af5ae09560faa
takuya matsumoto <takuya_matsumot_ai@mail.toyota.co.jp>
2026-03-19 13:04:59 +0900

    モック関数のtestを追加

--- src/testDoubles/Stub/test/RocketLauncher.test.ts
+++ src/testDoubles/Stub/test/RocketLauncher.test.ts
@@ -27,14 +27,11 @@ describe('RocketLauncherImpl（ロケット発射装置）のテスト', () => {
         expect(result).toBeFalsy()
     })
 
+    // vi.fn() を使って、モック関数を作成してみましょう。モック関数を使うと、より簡単に「晴れ」の状態を Stub できます。
     it('天気が「晴れ」の場合、打ち上げを実行すること(モック関数を使った場合)', async () => {
-        const weatherRepository = {
-            getWeather: vi.fn().mockReturnValue("SUNNY")
-        }
-        const rocketLauncher = new RocketLauncherImpl(weatherRepository)
+    })
+    // モック関数を使って、「雨」の状態も Stub してみましょう。
+    it('天気が「雨」の場合、打ち上げを中止すること(モック関数を使った場合)', async () => {
 
-        const result = await rocketLauncher.launch()
-
-        expect(result).toBeTruthy()
     })
 })
