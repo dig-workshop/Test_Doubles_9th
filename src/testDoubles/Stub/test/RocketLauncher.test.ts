40cf77b17b5bea8b5b1d395d57a252878d261a39
takuya matsumoto <takuya_matsumot_ai@mail.toyota.co.jp>
2026-03-24 18:08:12 +0900

    test追加。微修正

--- src/testDoubles/Stub/test/RocketLauncher.test.ts
+++ src/testDoubles/Stub/test/RocketLauncher.test.ts
@@ -5,7 +5,7 @@ describe('RocketLauncherImpl（ロケット発射装置）のテスト', () => {
 
 
     // 今の実装は WeatherRepositoryImpl を使っていますが、これはランダムに「晴れ」か「雨」を返すため、テストが安定しません。
-    // まずはこの「晴れ」の時のテストが通るように、stubSunnyWeatherRepository を実装し、「晴れ」の状態を Stubしよう
+    // まずはこの「晴れ」の時のテストが通るように、StubSunnyWeatherRepository を実装し、「晴れ」の状態を Stubしよう
     // 正しく Stub ができたら、まずは「晴れ」のテストが通るようにrocketLauncher の実装を修正しましょう。
     it('天気が「晴れ」の場合、打ち上げを実行すること', async () => {
         const weatherRepository = new WeatherRepositoryImpl()
@@ -16,7 +16,7 @@ describe('RocketLauncherImpl（ロケット発射装置）のテスト', () => {
         expect(result).toBeTruthy()
     })
 
-    // 今度は「雨」の時のテストをするために、stubRainyWeatherRepository を実装し、
+    // 今度は「雨」の時のテストをするために、StubRainyWeatherRepository を実装し、
     // 正しく Stub ができたら、どちらのテストも通るように、rocketLauncher を再度修正しましょう。
     it('天気が「雨」の場合、打ち上げを中止すること', async () => {
         const weatherRepository = new WeatherRepositoryImpl()
